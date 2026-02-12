f160de3a4895d68e1093d45e614f7a4c51e92e32
Josip Cavar <josipcavar04@gmail.com>
2026-02-12 12:37:11 +0100

    Migrate Linux support from unsafeFlags to SE-0482 artifact bundle
    Replace the Binary/ directory and RealtimeSanitizerCBindings target with
    an SE-0482 staticLibrary artifact bundle, eliminating unsafeFlags from
    Package.swift so this package can be used as a transitive dependency.
    
    - Create rtsan.artifactbundle with prebuilt librtsan.a for x86_64 and
      aarch64 Linux, bundled headers, and module map
    - Bump swift-tools-version to 6.2 and swift-syntax to 602.x
    - Update CI to Swift 6.2 and remove submodule checkout
    - Remove rtsan git submodule, .gitmodules, Binary/, and CBindings target

--- Package.swift
+++ Package.swift
@@ -1,14 +1,8 @@
-// swift-tools-version: 6.0
+// swift-tools-version: 6.2
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 import CompilerPluginSupport
-
-#if arch(x86_64)
-let path = "x86_64"
-#else
-let path = "aarch64"
-#endif
 
 let package = Package(
     name: "RTSanStandaloneSwift",
@@ -17,13 +11,17 @@ let package = Package(
         .library(name: "RealtimeSanitizer", targets: ["RealtimeSanitizer", "RealtimeSanitizerCore"])
     ],
     dependencies: [
-        .package(url: "https://github.com/swiftlang/swift-syntax.git", from: "600.0.0-latest")
+        .package(url: "https://github.com/swiftlang/swift-syntax.git", from: "602.0.0-latest")
     ],
     targets: [
         .binaryTarget(
             name: "rtsan",
             url: "https://github.com/realtime-sanitizer/rtsan-libs/releases/download/v20.1.1.2/rtsan.xcframework.zip",
             checksum: "f4f45abeec757adebc81f0c419da3aca0768357406cd7f8fee4c3a6289e50045"
+        ),
+        .binaryTarget(
+            name: "rtsan-linux",
+            path: "rtsan.artifactbundle"
         ),
         .macro(
             name: "RealtimeSanitizerMacros",
@@ -33,18 +31,10 @@ let package = Package(
             ]
         ),
         .target(
-            name: "RealtimeSanitizerCBindings",
-            path: "Sources/RealtimeSanitizerCBindings",
-            cSettings: [.headerSearchPath("../../rtsan/include/rtsan_standalone")]
-        ),
-        .target(
             name: "RealtimeSanitizerCore",
             dependencies: [
                 .target(name: "rtsan", condition: .when(platforms: [.macOS, .iOS, .tvOS, .watchOS, .macCatalyst])),
-                .target(name: "RealtimeSanitizerCBindings", condition: .when(platforms: [.linux]))
-            ],
-            linkerSettings: [
-                .unsafeFlags(["-L", "./Binary/\(path)", "-lrtsan"], .when(platforms: [.linux])),
+                .target(name: "rtsan-linux", condition: .when(platforms: [.linux]))
             ]
         ),
         .target(
