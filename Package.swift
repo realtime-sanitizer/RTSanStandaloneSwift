b46b785a344e737e1df381fb0d498a17aea0cabe
Josip Cavar <josipcavar04@gmail.com>
2026-02-13 09:16:50 +0100

    Bump swift-tools-version to 6.2 and swift-syntax to 602.x
    Required for SE-0482 artifact bundle support in the next PR.

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 6.0
+// swift-tools-version: 6.2
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -17,7 +17,7 @@ let package = Package(
         .library(name: "RealtimeSanitizer", targets: ["RealtimeSanitizer", "RealtimeSanitizerCore"])
     ],
     dependencies: [
-        .package(url: "https://github.com/swiftlang/swift-syntax.git", from: "600.0.0-latest")
+        .package(url: "https://github.com/swiftlang/swift-syntax.git", from: "602.0.0-latest")
     ],
     targets: [
         .binaryTarget(
